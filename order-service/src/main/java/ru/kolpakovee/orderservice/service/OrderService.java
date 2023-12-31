3de568b4f0aa5c502edeb7694e9e60f953784676
kolpakovee <eekolpakov@edu.hse.ru>
2023-12-31 12:10:39 +0300

    update OrderService

--- order-service/src/main/java/ru/kolpakovee/orderservice/service/OrderService.java
+++ order-service/src/main/java/ru/kolpakovee/orderservice/service/OrderService.java
@@ -13,13 +13,14 @@ import ru.kolpakovee.orderservice.repository.OrderRepository;
 import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
+import java.util.logging.Logger;
 
 @Service
 @RequiredArgsConstructor
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
 
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -38,8 +39,8 @@ public class OrderService {
                 .toList();
 
         // Call Inventory Service and place order if product is in stock
-        InventoryResponse[] inventoryResponses = webClient.get()
-                .uri("http://localhost:8082/api/inventory",
+        InventoryResponse[] inventoryResponses = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory",
                         uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
                 .retrieve()
                 .bodyToMono(InventoryResponse[].class)
